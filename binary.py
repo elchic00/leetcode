b39645bc0f7ba15e975849492c4f9664b11b1e2d
Andrew Alagna <aalagna04@gmail.com>
2021-10-15 11:00:15 -0400

    update

--- binary.py
+++ binary.py
@@ -1,11 +1,18 @@
-def binary(n):
+def decTobinary(n):
     if n > 1:
-        binary(n//2)
+        decTobinary1(n//2)
     s.append(n%2)
     return s
+def binaryToDecimal(head):
+        
+    num = head.val
+    while head.next:
+        num = num * 2 + head.next.val
+        head = head.next
+    return num
 
 if __name__ == "__main__":
     s = []
     n = int(input("Enter a number to convert to binary: "))
-    binary(n)  
+    decTobinary(n)  
     print("".join(str(x) for x in s))
